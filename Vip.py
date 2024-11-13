f82c68ae5f1af49fdf0b223ad6516595fc490d34
Laxmnytop <singhlsr38@gmail.com>
2024-11-13 08:44:05 +0530

    Add files via upload

---
+++ Vip.py
@@ -0,0 +1,17 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY Vip_Ddos_07
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7390699440:AAHJcbhKoL6QN4tjbCfrilQowAwka_eSCBE")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("LSR_RAJPUT")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1337204350")
+
+
+
+
+
+  
